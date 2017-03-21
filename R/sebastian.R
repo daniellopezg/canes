5d2677f6379f0368257465ef9255c5254b1c2ca0
Daniel Lopez <daniel.lopezg@hotmail.com>
2017-03-21 18:54:49 -0400

    first commit

---
+++ R/sebastian.R
@@ -0,0 +1,13 @@
+#'Title of the function, should be clear
+#'
+#'What does the function do
+#'@param x An integer
+#'@param y An integer
+#'@param z An integer
+#'@return The sum of \code{x} and \code{y} to the power of \code{z}
+#'@export
+#'@examples
+#'sebastian(1,2,3)
+sebastian<-function(x,y,z){
+  answer<-(x+y)^z
+}
